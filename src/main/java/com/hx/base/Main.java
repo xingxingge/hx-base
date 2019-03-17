c2c6808caa0601c5c9d90f5f740ab5f1a5bccbd1
huangxing <huangxing@gsafety.com>
2019-03-17 19:30:05 +0800

    加入docker插件

--- src/main/java/com/hx/base/Main.java
+++ src/main/java/com/hx/base/Main.java
@@ -3,5 +3,11 @@ package com.hx.base;
 public class Main {
   public static void main(String[] args) {
     System.out.println("==============");
+    Object obj = new Object();
+    try {
+      obj.wait();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
